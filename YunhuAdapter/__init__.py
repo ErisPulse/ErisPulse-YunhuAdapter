ead9fd74189d172476b145307914fa07373b83da
Auto <auto@builder.com>
2025-06-07 01:17:59 +0800

    build(YunhuAdapter): 优化模块构建流程并自动创建 PR

--- YunhuAdapter/__init__.py
+++ YunhuAdapter/__init__.py
@@ -1,11 +1,11 @@
 moduleInfo = {
     "meta": {
         "name": "YunhuAdapter",
-        "version": "2.0.2",
+        "version": "2.0.3",
         "description": "云湖协议适配器，整合所有云湖功能模块",
         "author": "r1a, WSu2059",
         "license": "MIT",
-        "homepage": "https://github.com/wsu2059q/ErisPulse-YunhuAdapter"
+        "homepage": "https://github.com/ErisPulse/ErisPulse-YunhuAdapter"
     },
     "dependencies": {
         "requires": [],
